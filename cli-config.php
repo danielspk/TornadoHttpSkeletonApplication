08c942f378208f048eb4beddcaf8adee6dafac20
Daniel Spiridione <daniel.spiridione@gmail.com>
2016-06-12 01:19:51 -0300

    Change migrations config

--- cli-config.php
+++ cli-config.php
@@ -1,11 +1,6 @@
 <?php
 
-use Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand;
-use Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand;
-use Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand;
-use Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand;
-use Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand;
-use Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand;
+use Doctrine\DBAL\Migrations\Configuration\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Setup;
@@ -13,27 +8,51 @@ use Dotenv\Dotenv;
 use Symfony\Component\Console\Helper\QuestionHelper;
 
 /**
- * Doctrine console
+ * Doctrine console configuration
  */
+
+// config by environment
 $dotenv = new Dotenv('src/App');
 $dotenv->load();
 
 $config = require 'src/App/config.php';
 
+// config entityManager
 $isDevMode = ($config['environment'] == 'develop');
-$setup = Setup::createAnnotationMetadataConfiguration($config['doctrine.orm']['entities.src'], $isDevMode);
-$entityManager = EntityManager::create($config['doctrine.orm']['db.params'], $setup);
+$emSetup = Setup::createAnnotationMetadataConfiguration($config['doctrine.orm']['entities.src'], $isDevMode);
+$entityManager = EntityManager::create($config['doctrine.orm']['db.params'], $emSetup);
 
+// create helperSet
 $helperSet = ConsoleRunner::createHelperSet($entityManager);
 $helperSet->set(new QuestionHelper(), 'dialog');
 
-$cli = ConsoleRunner::createApplication($helperSet, [
-    new DiffCommand(),
-    new ExecuteCommand(),
-    new GenerateCommand(),
-    new MigrateCommand(),
-    new StatusCommand(),
-    new VersionCommand(),
-]);
+// create console application
+$cli = ConsoleRunner::createApplication($helperSet);
 
+// config migration commands
+$migrationConfiguration = new Configuration($entityManager->getConnection());
+$migrationConfiguration->setMigrationsTableName('doctrine_migration_versions');
+$migrationConfiguration->setMigrationsNamespace('App\\Migrations');
+$migrationConfiguration->setMigrationsDirectory(__DIR__.'/src/App/Migrations');
+$migrationConfiguration->registerMigrationsFromDirectory(__DIR__.'/src/App/Migrations');
+
+// add migration commands to console application
+$migrationCommands = [
+    'DiffCommand',
+    'ExecuteCommand',
+    'GenerateCommand',
+    'MigrateCommand',
+    'StatusCommand',
+    'VersionCommand',
+];
+
+foreach($migrationCommands as $commandName) {
+    $fullCommandName = 'Doctrine\\DBAL\\Migrations\\Tools\\Console\\Command\\'.$commandName;
+    $command = new $fullCommandName();
+    $command->setMigrationConfiguration($migrationConfiguration);
+
+    $cli->add($command);
+}
+
+// run console
 return $cli->run();
